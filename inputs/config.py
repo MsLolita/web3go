788b07b36b7141d5984bccbd9839e58f9650aa83
MsLolita <oblikovuizapuc@gmail.com>
2024-01-10 17:33:41 +0200

    fix mobile proxy comment

--- inputs/config.py
+++ inputs/config.py
@@ -2,7 +2,7 @@ THREADS = 1  # Enter amount of threads
 CUSTOM_DELAY = (0, 1)  # delay before every registration in seconds
 
 # left empty if you use static proxies from file proxies.txt
-MOBILE_PROXY = ""  # login:password@ip:port - login1:a_password@2.56.119.93:5072
+MOBILE_PROXY = ""  # http://login:password@ip:port - login1:a_password@2.56.119.93:5072
 MOBILE_PROXY_CHANGE_IP_LINK = ""
 
 ###################################### left empty
